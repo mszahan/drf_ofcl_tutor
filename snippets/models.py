b95c1ff937dcf433081e467bb2d752cd67d5b160
mszahan <mszahan1@gmail.com>
2022-10-08 17:53:40 +0600

    added new owner and used serialized it in the view, ulr and serializer

--- snippets/models.py
+++ snippets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 
 
@@ -15,9 +17,25 @@ class Snippet(models.Model):
     linos = models.BooleanField()
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly',max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     def __str__(self):
         return self.title
+    
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linos = 'table' if self.linos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linos=linos,
+                                full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
     
     class Meta:
         ordering = ['created']

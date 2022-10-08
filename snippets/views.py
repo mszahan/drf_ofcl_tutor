6e402fd2e30c294ddf451c759ba4e87826fe33d0
mszahan <mszahan1@gmail.com>
2022-10-08 19:11:32 +0600

    added endpoint for the root_api and highlighted

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import generics
-from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
-from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
+
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 
 
@@ -36,10 +41,23 @@ class UserDetail(generics.RetrieveAPIView):
     serializer_class = UserSerializer
 
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
+# this views is just calling the highligheted text in the html format
+# need to understand this
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
 
-
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 
